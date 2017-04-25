4022d71608bf7d28ce04c125a75c866e827a47da
alchenist <alchenist@gmail.com>
2017-04-25 15:23:03 -0400

    Adds extract_intersections to data/make.py

--- src/data/make.py
+++ src/data/make.py
@@ -4,8 +4,9 @@ import join_segments_crash_concern
 import make_canon_dataset
 
 
-# TODO: Decide on some sort of analysis pipeline framework or tool: Makefiles? Luigi/Airflow? Drain?
-extract_intersections.main("data/raw/ma_cob_spatially_joined_streets.shp", out_pickle = "data/interim/inters.pkl", out = "data/interim/inters.shp")
-create_segments.main()
-join_segments_crash_concern.main()
-make_canon_dataset.main()
+if __name__ == '__main__':
+    # TODO: Decide on some sort of analysis pipeline framework or tool: Makefiles? Luigi/Airflow? Drain?
+    extract_intersections.main("data/raw/ma_cob_spatially_joined_streets.shp", out_pickle = "data/interim/inters.pkl", out = "data/interim/inters.shp")
+    create_segments.main()
+    join_segments_crash_concern.main()
+    make_canon_dataset.main()
